f53e1c5dd3dca5dcd04d1ce42f033fae6325ca33
vanhoa690 <vanhoa690@gmail.com>
2026-07-16 17:02:15 +0700

    call api post

--- src/labs/Lab5.tsx
+++ src/labs/Lab5.tsx
@@ -1,7 +1,26 @@
-import { Button, Form, Input } from "antd";
+import { useMutation } from "@tanstack/react-query";
+import { Button, Form, Input, message } from "antd";
+import axios from "axios";
 
 function Lab5() {
-  const onFinish = () => {};
+  // useMutation: POST data
+  const { mutate } = useMutation({
+    mutationFn: async (data) => {
+      await axios.post("http://localhost:3000/stories", data);
+    },
+    onSuccess: () => {
+      message.success("Them thanh cong");
+      // nav sang list
+    },
+    onError: () => {
+      message.error("Them that bai");
+    },
+  });
+
+  const onFinish = (values: any) => {
+    console.log(values);
+    mutate(values);
+  };
   return (
     <div>
       <h2>Lab5</h2>
