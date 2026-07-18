f5dbf04a1a97d556d4c1ef3e746e73ea5433b3ec
vanhoa690 <vanhoa690@gmail.com>
2026-07-18 17:30:21 +0700

    edit story

--- src/labs/Lab6.tsx
+++ src/labs/Lab6.tsx
@@ -1,7 +1,29 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { Button, Form, Input } from "antd";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 function Lab6() {
-  const onFinish = () => {};
+  // 1. lay id
+  const { id } = useParams();
+
+  // 2. lay data theo id
+  const { data } = useQuery({
+    queryKey: [],
+    queryFn: () => {},
+  });
+
+  // 3. fill Form
+  useEffect(() => {}, []);
+
+  // 4. call api
+  const { mutate } = useMutation({
+    mutationFn: () => {},
+  });
+
+  const onFinish = (values: any) => {
+    mutate(values);
+  };
   return (
     <div>
       <h2>Lab 6</h2>
